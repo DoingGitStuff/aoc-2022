9b6f1ece280fd3dc79ecef60fc3d129046ed0477
DoingGitStuff <78906194+DoingGitStuff@users.noreply.github.com>
2022-12-01 20:22:28 +0000

    added mix task solutions to get the solutions

---
+++ elixir_aoc/lib/mix/tasks/solutions.ex
@@ -0,0 +1,33 @@
+defmodule Mix.Tasks.Solutions do
+  use Mix.Task
+
+  @solutions %{
+    "1" => ElixirAoc.One,
+  }
+
+  def run(args) do
+    solutions_keys =
+      @solutions
+      |> Map.keys()
+
+    run_order =
+      solutions_keys
+      |> Enum.filter(&(length(args) == 0 or &1 in args))
+      |> :lists.sort()
+
+    for k <- run_order do
+      mod = Map.get(@solutions,k)
+      input_file = apply(mod,:input_file,[])
+      priv_dir = Path.join([".","priv"])
+      filepath = Path.join([priv_dir,input_file])
+
+      exists = File.exists?(filepath)
+      IO.puts("#{filepath} exists=#{exists}")
+      if exists do
+        {:ok,input_txt} = File.read(filepath)
+        input = String.trim(input_txt)
+        IO.puts("#{k} = #{apply(mod,:solve,[input])}")
+      end
+    end
+  end
+end
