809075b53600968a237d653c72794517abc61bea
DoingGitStuff <78906194+DoingGitStuff@users.noreply.github.com>
2022-12-06 11:38:17 +0000

    removed dead code

--- elixir_aoc/lib/elixir_aoc/five.ex
+++ elixir_aoc/lib/elixir_aoc/five.ex
@@ -23,47 +23,6 @@ defmodule ElixirAoc.Five do
     {header, Enum.drop_while(lines, &(not String.starts_with?(&1, "move")))}
   end
 
-  def parse_header(lines) do
-    header_lines =
-      lines
-      |> Enum.take_while(fn line -> not String.starts_with?(line, " 1") end)
-
-    len = length(header_lines)
-    rest = Enum.drop(lines, len)
-
-    stacks =
-      header_lines
-      |> Enum.map(&parse_header_line/1)
-      |> Enum.reverse()
-      |> Enum.reduce(&merge_stk_lines/2)
-
-    # nums = List.last(header_lines)
-
-    {stacks, rest}
-  end
-
-  def parse_header_line(line) do
-    indexed_chunks =
-      String.to_charlist(line)
-      |> Enum.chunk_every(4)
-      |> Stream.with_index()
-
-    stk_line =
-      indexed_chunks
-      |> Enum.map(fn {chunk, idx} -> {Enum.at(chunk, 1), idx + 1} end)
-      |> Enum.filter(fn {c, _idx} -> c != ?\s end)
-
-    stacks = Enum.count(indexed_chunks)
-    start = List.duplicate([], stacks) ++ [Enum.map(stk_line, fn {c, _idx} -> c end)]
-    instrs = Enum.map(stk_line, fn {_c, idx} -> {stacks + 1, idx} end)
-    Enum.reduce(instrs, start, &eval_instr/2) |> Enum.take(stacks)
-  end
-
-  def merge_stk_lines(line, acc) do
-    Enum.zip(line, acc)
-    |> Enum.map(fn {l, a} -> l ++ a end)
-  end
-
   def parse_instr("move " <> x_from_a_to_b) do
     {x, " from " <> a_to_b} = Integer.parse(x_from_a_to_b)
     {a, " to " <> rest} = Integer.parse(a_to_b)
